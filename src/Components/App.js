82755007599204ed07c8554c38b92aa0dc2ece4e
Kartikey06dk <kartikeysingh253@gmail.com>
2023-09-16 12:07:48 +0530

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import InputField from "./InputField";
+import "../style.css";
+
+const App = () => {
+    return (
+        <div>
+            <InputField />
+        </div>
+    )
+}
+
+export default App;
